5c2e9f0d283418913bbf267ff97117b60e4e1a94
abdurrahim220 <z.abdurrahim5@gmail.com>
2023-01-26 15:34:30 +0600

    chin chin

---
+++ JavaScript/Recap_Js/output.js
@@ -0,0 +1,4 @@
+console.log("I am learning javaScript!!");
+console.log("I am learning javaScript!!");
+
+document.write("This is a document output crack!!!")
---
+++ JavaScript/Recap_Js/syntex.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync')();
+
+
+// var x ;
+// x = prompt("Enter Your name: ");
+
+// console.log(x)
+
+
+let a;
+var b;
+
+a = "Rahim"
+
+b = 45;
+
+console.log(a)
+console.log(b)
