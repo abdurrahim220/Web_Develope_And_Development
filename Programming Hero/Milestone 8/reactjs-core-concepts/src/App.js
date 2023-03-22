8515e6c7a3df87d22f2e183b8d12bc53911e0e45
abdurrahim220 <abdurrahim5@gmail.com>
2023-03-22 08:30:47 +0600

    react js First

--- Programming Hero/Milestone 8/reactjs-core-concepts/src/App.js
+++ Programming Hero/Milestone 8/reactjs-core-concepts/src/App.js
@@ -1,48 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
 
+const singers = [
+  { name: 'dr.Mahfuz', job: "Singer" },
+  { name: 'Eva Rahman', job: "Singer2" },
+  { name: 'Agun', job: "Sopno" },
+  { name: 'Shuvro', job: "pathor" }
+]
+
 function App() {
+  const nayoks = ['Rubel', 'Baparaz', 'Kubir', 'Rahim', 'Bindaas', 'Jamil', 'Rafik', 'Riyaz', 'Razzak', 'Ashik', 'Anwar']
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      {
+        nayoks.map(nayok => <li>{nayok}</li>)
+      }
+      {/* <Person name={nayoks[0]} nayika="Moushomi"></Person> */}
+      {/* <Person name={nayoks[1]} nayika="Cheka"></Person> */}
+      {/* <Person name={nayoks[2]} nayika="Kopila"></Person> */}
+
+      {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      }
 
 
-    <h5>New Component. YAY</h5>
-    <p>Rock mama React Mama</p>
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
 
-      <Friend></Friend>
-      <Friend></Friend>
-      <Friend></Friend>
-      <Rahim></Rahim>
-      <Rahim></Rahim>
+
+      <h5>New Component. YAY</h5>
+      <p>Rock mama React Mama</p>
+
+      <Friend phone="01777" movie="Singam"></Friend>
+      <Friend phone="0173377" movie="Rahim"></Friend>
+
     </div>
   );
 }
 
 
 function Person(props) {
-  console.log(props);
+  // console.log(props);
   return (
     <div className='person'>
-      <h1>Sakib Al Hasan</h1>
-      <p>Professon: Cricket</p>
+      {/* <h1>Sakib Al Hasan</h1> */}
+      <h1>Name: {props.name}</h1>
+      {/* <p>Profession: Cricket</p> */}
+      <h1>Nayika : {props.nayika}</h1>
     </div>
   )
 };
 
-function Friend() {
+function Friend(props) {
+  console.log(props);
   return (
     <div className='container'>
-      <h3>Name : Ajay devgan</h3>
-      <p>Job: Fight</p>
+      <h3>Name : {props.movie}</h3>
+      <p>Phone: {props.phone}</p>
     </div>
   )
 }
 
-function Rahim(){
+function Rahim() {
   return (
     <div className='person'>
       <h3>Name : Md Abdur Rahim</h3>
