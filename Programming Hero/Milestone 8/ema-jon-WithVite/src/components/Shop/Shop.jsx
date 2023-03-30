6e10812b51d333b4e4804ad75925f8d7bac2a6a5
abdurrahim220 <abdurrahim5@gmail.com>
2023-03-30 09:21:58 +0600

    cart details

--- Programming Hero/Milestone 8/ema-jon-WithVite/src/components/Shop/Shop.jsx
+++ Programming Hero/Milestone 8/ema-jon-WithVite/src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../cart/Cart';
 import Product from '../product/Product';
 import './Shop.css'
 
@@ -31,8 +32,9 @@ const Shop = () => {
                }
             </div>
             <div className='cart-container'>
-                <h3>Product Summery</h3>
-                <p>Selected Item : {cart.length}</p>
+                {/* <h3>Product Summery</h3>
+                <p>Selected Item : {cart.length}</p> */}
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
---
+++ Programming Hero/Milestone 8/ema-jon-WithVite/src/components/cart/Cart.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = ({ cart }) => {
+    //const cart = props.cart; //!option 1
+    //const {cart} = props; //!option 2
+
+    let total = 0;
+    let cartShipping = 0;
+    for(const product of cart)
+    {
+        total += product.price;
+        cartShipping += product.shipping;
+    }
+    const tax = total * 7/100;
+    const grandTotal = total + cartShipping + tax;
+    return (
+        <div className='cart'>
+            <h3>Order Summery</h3>
+            <p>Selected Item : ${cart.length}</p>
+            <p>Total Price : ${total.toFixed(2)}</p>
+            <p>Total Shipping : ${cartShipping.toFixed(2)}</p>
+            <p>Tax : ${tax.toFixed(2)}</p>
+            <p>Grand Total : ${grandTotal.toFixed(2)}</p>
+        </div>
+    );
+};
+
+export default Cart;
