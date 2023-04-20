6168289e71b65adc7ba419cb280d2e952f77f6ef
abdurrahim220 <z.abdurrahim5@.com>
2023-04-20 18:09:29 +0600

    login form

--- Programming Hero/Milestone 10/email-password-auth/src/component/Login/Login.jsx
+++ Programming Hero/Milestone 10/email-password-auth/src/component/Login/Login.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        setError('');
+        setSuccess('');
+        const form = event.target;
+
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email,password);
+        
+    }
     return (
         <div>
+            <h2>Please login</h2>
             <form>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input type="text" className="form-control" id="username" placeholder="Enter username" />
+                    <input type="text" className="form-control" name='email' id="username" placeholder="Enter username" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" id="password" placeholder="Password" />
+                    <input type="password" className="form-control" name='password' id="password" placeholder="Password" required />
                 </div>
                 <button type="submit" className="btn btn-primary">Login</button>
             </form>
