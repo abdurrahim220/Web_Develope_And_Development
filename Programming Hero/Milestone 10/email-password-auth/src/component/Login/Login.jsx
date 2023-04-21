2beb20c11422216307bd0374b03e4809187e35d8
abdurrahim220 <z.abdurrahim5@.com>
2023-04-21 06:07:41 +0600

    recap user login

--- Programming Hero/Milestone 10/email-password-auth/src/component/Login/Login.jsx
+++ Programming Hero/Milestone 10/email-password-auth/src/component/Login/Login.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
+import app from '../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const auth = getAuth(app)
 
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
+    const emailRef = useRef();
 
     const handleSubmit = event =>{
         event.preventDefault();
@@ -15,15 +20,74 @@ const Login = () => {
         const password = form.password.value;
 
         console.log(email,password);
-        
+         // password validation
+         if (!/(?=.*[0-9])/.test(password)) {
+            setError('Please Add a single Digit!');
+            return;
+        }
+        else if(!/(?=.*[A-Z])/.test(password)){
+            setError("Please Add a UpperCase letter!")
+            return;
+        }
+        else if(!/(?=.*\W)/.test(password)){
+            setError("Please Add a Special Letter!!!");
+            return;
+        }
+        else if(password.length <7){
+            setError("Password Must container 7 letters !");
+            return;
+        }
+        // firebase
+        createUserWithEmailAndPassword(auth,email,password)
+        .then(result =>{
+            const usserLogin = result.user;
+            console.log(usserLogin);
+            setSuccess("You log in successfull");
+            setError('')
+            
+        })
+        .catch(error =>{
+            console.log(error.message);
+            setError(error.message);
+        });
+        signInWithEmailAndPassword(auth,email,password)
+            .then(result => {
+                const logginedUser = result.user;
+                setSuccess('Log in successfull');
+                setError("")
+            })
+            .catch(error =>{
+                setError(error.message)
+            });
+           
+    }
+    const handleResetPassword = event =>{
+        const email = (emailRef.current.value);
+        console.log(emailRef.current.value);
+        if(!email)
+        {   
+            alert("Pleaser provide your email to reset password!!");
+            return;
+           
+        }
+        sendPasswordResetEmail(auth,email)
+        .then(() =>{
+            alert("Please check your email")
+            
+
+        })
+        .catch( error =>{
+            console.log(error.message);
+            setError(error.message)
+        })
     }
     return (
         <div>
             <h2>Please login</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input type="text" className="form-control" name='email' id="username" placeholder="Enter username" required />
+                    <input type="text" className="form-control" ref={emailRef} name='email' id="username" placeholder="Enter username" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
@@ -31,6 +95,10 @@ const Login = () => {
                 </div>
                 <button type="submit" className="btn btn-primary">Login</button>
             </form>
+            <p><small>If you are new? please <Link to='/register'>register</Link></small></p>
+            <p>Forget Password ? please<button onClick={handleResetPassword} className='btn btn-link'>Reset Password</button></p>
+            <p className='text-danger'>{error}</p>
+            <p className='text-success'>{success}</p>
         </div>
     );
 };
--- Programming Hero/Milestone 10/email-password-auth/src/component/Register/Register.jsx
+++ Programming Hero/Milestone 10/email-password-auth/src/component/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, signInWithEmailAndPassword, updateCurrentUser, updateProfile } from "firebase/auth";
 import app from '../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
 
 
 const Register = () => {
@@ -18,21 +19,22 @@ const Register = () => {
         //collect form data
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(email, password);
+        const name = event.target.name.value;
+        console.log(name,email, password);
         // password validation
         if (!/(?=.*[0-9])/.test(password)) {
             setError('Please Add a single Digit!');
             return;
         }
-        else if(!/(?=.*[A-Z])/.test(password)){
+        else if (!/(?=.*[A-Z])/.test(password)) {
             setError("Please Add a UpperCase letter!")
             return;
         }
-        else if(!/(?=.*\W)/.test(password)){
+        else if (!/(?=.*\W)/.test(password)) {
             setError("Please Add a Special Letter!!!");
             return;
         }
-        else if(password.length <7){
+        else if (password.length < 7) {
             setError("Password Must container 7 letters !");
             return;
         }
@@ -44,13 +46,16 @@ const Register = () => {
                 console.log(loggedUser);
                 setError('');
                 event.target.reset();
-                setSuccess("user has created successfully")
+                setSuccess("user has created successfully");
+                verifyEmail(result.user)
+                updateUser(result.user,name)
             })
             .catch(error => {
                 console.log(error.message);
                 setError(error.message);
 
-            })
+            });
+
     }
     const handleEmailChange = (event) => {
         // console.log(event.target.value);
@@ -60,16 +65,36 @@ const Register = () => {
     const handlePasswordBlur = (event) => {
         // console.log(event.target.value);
     }
+    // verification email
+    const verifyEmail = user => {
+        sendEmailVerification(user)
+            .then(result => {
+                console.log(result);
+                alert("Please verify your email");
+
+            })
+
+    };
+    const updateUser=(user,name) =>{
+        updateProfile(user,{
+            displayName:name
+        })
+        .then(() =>console.log("Profile updated!"))
+        .catch(error => setError(error.message))
+    } 
 
     return (
         <div className='w-50 mx-auto'>
             <h5>This is register</h5>
             <form onSubmit={handleSubmit}>
-                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' required />
+                <input className='mb-3'  type="text" name="name" id="name" placeholder='Your Name' required />
                 <br />
-                <input onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' required />
+                <input className='mb-3' onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' required />
+                <br />
+                <input className='mb-3' onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' required />
                 <br />
                 <input type="submit" value="Register" />
+                <p><small>If you already have an account ? please <Link to='/login'>Log in</Link></small></p>
                 <p className='text-danger'>{error}</p>
                 <p className='text-success'>{success}</p>
             </form>
