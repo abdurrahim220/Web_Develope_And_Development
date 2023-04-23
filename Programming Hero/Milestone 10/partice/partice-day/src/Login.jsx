afa6c4baab1c541ed978cd5b655657965249462a
abdurrahim220 <z.abdurrahim5@.com>
2023-04-23 23:40:26 +0600

    Partice day

--- Programming Hero/Milestone 10/partice/partice-day/src/Login.jsx
+++ Programming Hero/Milestone 10/partice/partice-day/src/Login.jsx
@@ -1,26 +1,44 @@
 import React, { useState } from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import app from './firebase.config';
 import { Result } from 'postcss';
+import { Link } from 'react-router-dom';
 
 
 const Login = () => {
     const auth = getAuth(app);
     const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
     const [user, setUser] = useState(null)
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
+
+    // email login section
     const handleSubmit = (event) => {
         event.preventDefault();
         setError('');
         setSuccess('');
-        event.reset();
+        
         const form = event.target;
         const email = form.email.value;
-        const password = form.email.value;
+        const password = form.password.value;
+
+        signInWithEmailAndPassword(auth,email,password)
+        .then(result =>{
+            const loginEP = result.user;
+            // setUser(loginEP)
+            console.log(loginEP);
+            // form.reset();
+            setError('');
+            setSuccess("Successfully log In")
+        })
+        .catch(error =>{
+            setError(error.message)
+        })
 
     }
 
+    // social login
     const handleGoogle = () => {
 
         signInWithPopup(auth, googleProvider)
@@ -36,6 +54,18 @@ const Login = () => {
             })
     };
 
+    const handleGithub = () => {
+        signInWithPopup(auth, githubProvider)
+            .then(result => {
+                const loginGit = result.user;
+                console.log(loginGit);
+                setUser(loginGit)
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
+    }
+
     const handleSignOut = () => {
         signOut(auth)
             .then(result => {
@@ -45,6 +75,7 @@ const Login = () => {
                 console.log(error.message);
             })
     }
+
 
     return (
         <div>
@@ -56,25 +87,58 @@ const Login = () => {
                     </div>
                     <form onSubmit={handleSubmit} className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
 
-                        <div className="form-control mt-6">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required/>
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required/>
+                                <label className="label grid">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    <br />
+                                    <Link to='/register' className="label-text-alt link link-hover">If new please ? Register</Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </div>
+
+
+
+                        <div className="form-control mt-6 mx-auto text-center mb-2">
                             <h4>Social login</h4>
                             {user ?
                                 <button onClick={handleSignOut} className='btn btn-danger'>Sign Out</button> :
 
-                                <button onClick={handleGoogle} className="btn btn-primary">Google</button>}
+                                <div className='mx-auto'>
+                                    <button onClick={handleGoogle} className="btn btn-primary mr-2">Google</button>
+                                    <button onClick={handleGithub} className="btn btn-primary">GitHub</button>
+                                </div>
+                            }
                         </div>
 
                         {user && <div>
                             <p>{user.displayName}</p>
                             <p>{user.email}</p>
-                            <img src={user.photoURL}/>
-                            </div>}
+                            <img src={user.photoURL} />
+                        </div>}
 
 
                     </form>
-                </div>
-            </div>
-        </div>
+                    <div className='mx-auto'>
+                        <p className='text-red-400'>{error}</p>
+                        <p className='text-success'>{success}</p>
+                        </div>
+                </div >
+            </div >
+        </div >
     );
 };
 
--- Programming Hero/Milestone 10/partice/partice-day/src/Register.jsx
+++ Programming Hero/Milestone 10/partice/partice-day/src/Register.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import React, { useState } from 'react';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, sendEmailVerification } from "firebase/auth";
 import app from './firebase.config';
+import { Link } from 'react-router-dom';
 
 
 const Register = () => {
@@ -8,15 +9,52 @@ const Register = () => {
     const googleProvider = new GoogleAuthProvider();
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
-    const handleRegister = event =>{
+    const handleRegister = event => {
         event.preventDefault();
 
-        event.reset();
+        // event.reset();
 
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        
+        console.log(email, password);
+        setError('')
+        setSuccess('')
+
+
+        // password validation
+
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError("Please add one upper case letter !!");
+            return;
+        }
+        else if (!/(?=.*[!@#$&*])/.test(password)) {
+            setError("Please add one special character!");
+            return;
+        }
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const loginUser = result.user;
+                console.log(loginUser);
+                setSuccess("Successfully Register!!");
+                form.reset();
+                emailVerify(result.user)
+
+            })
+            .catch(error => {
+                setError(error.message);
+                console.log(error.message);
+            })
+
+
+    };
+    const emailVerify = (email) => {
+        sendEmailVerification(email)
+            .then(result => {
+                console.log(result);
+                alert("Please Verify");
+            })
 
     }
 
@@ -26,28 +64,32 @@ const Register = () => {
                 <div className="hero-content flex-col ">
                     <div className="text-center ">
                         <h1 className="text-5xl font-bold">Register now!</h1>
-
                     </div>
+
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
+                        <form onSubmit={handleRegister} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                                 <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    <Link to="/login" className="label-text-alt link link-hover">If you already have an account?</Link>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Register</button>
                             </div>
+                        </form>
+                        <div className='mx-auto'>
+                            <p className='text-red-400'>{error}</p>
+                            <p className='text-success'>{success}</p>
                         </div>
                     </div>
                 </div>
