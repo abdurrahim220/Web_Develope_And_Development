fac2789924f6a90ec38bc72bf8bbfa1fae2ae008
abdurrahim220 <abdurrahim5@gmail.com>
2023-05-15 10:34:55 +0600

    car services

---
+++ Programming Hero/Milestone11/car-doctor-client/src/page/Services/Services.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import ServicesCard from './ServicesCard';
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => {
+                setServices(data)
+            })
+    }, [])
+
+    return (
+        <div className='mb-5'>
+            <div className='space-y-3 text-center'>
+                <h3 className="text-3xl text-orange-400">
+                    Our Services
+                </h3>
+                <h2 className='text-5xl font-bold'>Our Service Area</h2>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {
+                    services.map(service => <ServicesCard
+                    key={service._id} service={service}
+                    ></ServicesCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
